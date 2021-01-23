eec7362b5fd0844c92b32006dd06546a2ff6b9fb
DigVijayWa <digvijaywadkar24@gmail.com>
2021-01-23 23:27:05 +0530

    level wise display

--- index.js
+++ index.js
@@ -10,54 +10,59 @@ async function fetchHTML(url) {
   return cheerio.load(data);
 }
 
-async function loadAllUrl(pageSize, firstUrl) {
+async function loadAllUrl(pageSize, firstUrl, level) {
   let mapArray = [];
   for (var i = 1; i <= pageSize / 10; i++) {
-    const data = await getPageDetails(firstUrl, i);
+    const data = await getPageDetails(firstUrl, i, level);
     mapArray = mapArray.concat(data);
   }
   return mapArray;
 }
 
-async function getPageDetails(url, i) {
+async function getPageDetails(url, i, level) {
   let pageItemArray = [];
-
   const data = await fetchHTML(url + "?p=" + i);
   data(".im-title").find(".im-usage").remove().text();
   let size = data(".im-title").find("a").length;
   for (var j = 0; j < size; j++) {
     var text = data(".im-title").find("a")[j].children[0]["data"];
     var link = data(".im-title").find("a")[j].attribs.href;
-    var dataArr = await loadPageSize(baseUrl + link + "/usages");
+    var dataArr = await loadPageSize(baseUrl + link + "/usages", level-1);
     pageItemArray.push({ link, text, data: dataArr });
   }
   return pageItemArray;
 }
 
-function resolveTransientDependencies(url) {}
-
-async function loadPageSize(url) {
-  const data = await fetchHTML(url);
-  let pageSize = findOutPageSize(data("h1").text());
-  if (pageSize > 0) {
-    const map = await loadAllUrl(pageSize, url);
-    return toStringMap(map);
+async function loadPageSize(url, level) {
+  if (level > 0) {
+    const data = await fetchHTML(url);
+    let pageSize = findOutPageSize(data("h1").text());
+    if (pageSize > 0) {
+      const map = await loadAllUrl(pageSize, url, level);
+      return map;
+    } else {
+      return [];
+    }
   } else {
-    return "[]";
+    return [];
   }
 }
 
 (async () => {
-  console.log("before start");
-
   let data = await loadPageSize(
-    "https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-cloudfoundry-connector/usages"
+    "https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-cloudfoundry-connector/usages",
+    2
   );
-  fs.writeFileSync("mod.json", '"spring-cloud-cloudfoundry-connector":' + data);
-  console.log("after start");
+  fs.writeFileSync(
+    "mod.json",
+    '{"spring-cloud-cloudfoundry-connector":' + toStringMap(data) + "}"
+  );
 })();
 
 function toStringMap(mapArray) {
+  if (mapArray.length == 0) {
+    return "[]";
+  }
   let stringBuilder = "[";
 
   mapArray.forEach((element) => {
@@ -68,7 +73,9 @@ function toStringMap(mapArray) {
       '",' +
       '"text":"' +
       element.text +
-      '"' +
+      '",' +
+      `"${element.text}":` +
+      toStringMap(element.data) +
       "},";
   });
 
