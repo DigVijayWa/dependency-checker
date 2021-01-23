c6183a05b9f8d4ced525cf7bb3bf7f7ae855b900
DigVijayWa <digvijaywadkar24@gmail.com>
2021-01-23 22:48:25 +0530

    async await

--- index.js
+++ index.js
@@ -1,95 +1,83 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-const fs = require('fs');
+const fs = require("fs");
 
-const baseUrl = "https://mvnrepository.com/artifact";
+const baseUrl = "https://mvnrepository.com";
 
-function fetchHTML(url) {
-    return new Promise((resolve, reject)=>{
-        axios.get(url).then(({data})=> {
-            resolve(cheerio.load(data));
-        }).catch(err => reject(err));
-    });
+async function fetchHTML(url) {
+  console.log(url);
+  const { data } = await axios.get(url);
+  return cheerio.load(data);
 }
 
-let mapArray = [];
-function loadAllUrl(pageSize, firstUrl) {
-    
-    
-    return new Promise((resolve, reject) => {
-        for(var i=1;i<pageSize/10; i++) {
-            getPageDetails(firstUrl, i).then(data=> {
-                mapArray = mapArray.concat(data);
-                if(mapArray.length >= pageSize-2) {
-                    resolve(mapArray);
-                }
-            });
-        }
-    });
+async function loadAllUrl(pageSize, firstUrl) {
+  let mapArray = [];
+  for (var i = 1; i <= pageSize / 10; i++) {
+    const data = await getPageDetails(firstUrl, i);
+    mapArray = mapArray.concat(data);
+  }
+  return mapArray;
 }
 
-function getPageDetails(url, i) {
-    let pageItemArray = [];
-    return new Promise((resolve, reject) => {
+async function getPageDetails(url, i) {
+  let pageItemArray = [];
 
-        fetchHTML(url+'?p='+i).then(data=>{
-            data('.im-title').find('.im-usage').remove().text();
-            let size = data('.im-title').find('a').length;
-            for(var j=0;j<size;j++) {
-                var text =  data('.im-title').find('a')[j].children[0]['data'];
-                var link = data('.im-title').find('a')[j].attribs.href;
-                pageItemArray.push({ link, text});
-                loadPageSize(baseUrl+link).then(function(resp){
-
-                }.bind({text}));
-            }
-            resolve(pageItemArray);
-        });
-    })
+  const data = await fetchHTML(url + "?p=" + i);
+  data(".im-title").find(".im-usage").remove().text();
+  let size = data(".im-title").find("a").length;
+  for (var j = 0; j < size; j++) {
+    var text = data(".im-title").find("a")[j].children[0]["data"];
+    var link = data(".im-title").find("a")[j].attribs.href;
+    var dataArr = await loadPageSize(baseUrl + link + "/usages");
+    pageItemArray.push({ link, text, data: dataArr });
+  }
+  return pageItemArray;
 }
 
-function resolveTransientDependencies(url) {
+function resolveTransientDependencies(url) {}
 
+async function loadPageSize(url) {
+  const data = await fetchHTML(url);
+  let pageSize = findOutPageSize(data("h1").text());
+  if (pageSize > 0) {
+    const map = await loadAllUrl(pageSize, url);
+    return toStringMap(map);
+  } else {
+    return "[]";
+  }
 }
 
-function loadPageSize(url) {
-    return new Promise((resolve, reject) => {
-        fetchHTML(url).then(data=> {
-            let pageSize = findOutPageSize(data('h1').text());
-            if(pageSize > 0) {
-                loadAllUrl(pageSize, url).then(map=> {
-                    resolve(toStringMap(map));
-                });
-            }
-            else {
-                resolve("[]");
-            }
-        });
-    });
-}
+(async () => {
+  console.log("before start");
 
-
-
-loadPageSize("https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-cloudfoundry-connector/usages").then(data=>{
-    console.log('"spring-cloud-cloudfoundry-connector":'+data);
-});
+  let data = await loadPageSize(
+    "https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-cloudfoundry-connector/usages"
+  );
+  fs.writeFileSync("mod.json", '"spring-cloud-cloudfoundry-connector":' + data);
+  console.log("after start");
+})();
 
 function toStringMap(mapArray) {
-    let stringBuilder = '[';
+  let stringBuilder = "[";
 
-    mapArray.forEach(element => {
-        stringBuilder += '{'+ 
-        '"link":"'+element.link+'",'+
-        '"text":"'+element.text+'"'+
-        '},'
-    });
+  mapArray.forEach((element) => {
+    stringBuilder +=
+      "{" +
+      '"link":"' +
+      element.link +
+      '",' +
+      '"text":"' +
+      element.text +
+      '"' +
+      "},";
+  });
 
-    stringBuilder = stringBuilder.substring(0,stringBuilder.length-1);
-    stringBuilder += ']';
+  stringBuilder = stringBuilder.substring(0, stringBuilder.length - 1);
+  stringBuilder += "]";
 
-    return stringBuilder;
+  return stringBuilder;
 }
 
 function findOutPageSize(text) {
-    return parseInt(text.split("(")[1].split(")")[0]);;
+  return parseInt(text.split("(")[1].split(")")[0]);
 }
