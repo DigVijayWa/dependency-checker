42d5cd6cace5327f93767043ce1fc2628f8c18fd
DigVijayWa <digvijaywadkar24@gmail.com>
2021-01-24 11:59:15 +0530

    checkList changes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
+const { format } = require("path");
+const checkList = require("./checList");
 
 const baseUrl = "https://mvnrepository.com";
 
@@ -12,7 +14,15 @@ async function fetchHTML(url) {
 
 async function loadAllUrl(pageSize, firstUrl, level) {
   let mapArray = [];
-  for (var i = 1; i <= pageSize / 10; i++) {
+  let pages = pageSize / 10;
+  let itemCount = pageSize % 10;
+  var i = 1;
+  for (i = 1; i <= pageSize / 10; i++) {
+    const data = await getPageDetails(firstUrl, i, level);
+    mapArray = mapArray.concat(data);
+  }
+
+  if (itemCount > 0) {
     const data = await getPageDetails(firstUrl, i, level);
     mapArray = mapArray.concat(data);
   }
@@ -27,7 +37,7 @@ async function getPageDetails(url, i, level) {
   for (var j = 0; j < size; j++) {
     var text = data(".im-title").find("a")[j].children[0]["data"];
     var link = data(".im-title").find("a")[j].attribs.href;
-    var dataArr = await loadPageSize(baseUrl + link + "/usages", level-1);
+    var dataArr = await loadPageSize(baseUrl + link + "/usages", level - 1);
     pageItemArray.push({ link, text, data: dataArr });
   }
   return pageItemArray;
@@ -53,11 +63,54 @@ async function loadPageSize(url, level) {
     "https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-cloudfoundry-connector/usages",
     2
   );
+
   fs.writeFileSync(
     "mod.json",
     '{"spring-cloud-cloudfoundry-connector":' + toStringMap(data) + "}"
   );
+
+  console.log("search starting");
+
+  checkList.forEach((item) => {
+    findDependency(data, item);
+  });
+
+  console.log("search complete ...");
 })();
+
+String.prototype.replaceOccurringDashes = function () {
+  let stringBuilder = "";
+  let flagDown = false;
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] != "-") {
+      flagDown = false;
+      stringBuilder += this[i];
+    } else if (!flagDown && this[i] == "-") {
+      flagDown = true;
+      stringBuilder += this[i];
+    }
+  }
+
+  return stringBuilder;
+}
+
+function findDependency(mapArray, item) {
+  if (mapArray.length == 0) {
+    return;
+  }
+  mapArray.forEach((element) => {
+    
+    let formatText = element.text.toLowerCase().replaceAll(/\s|::|\t/g, "-").replaceOccurringDashes();
+
+    if (item == formatText) {
+      console.log(formatText + " dependency is causing the issue");
+    }
+
+    if (element.data.length > 0) {
+      findDependency(element.data, item);
+    }
+  });
+}
 
 function toStringMap(mapArray) {
   if (mapArray.length == 0) {
